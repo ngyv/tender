1f8daa5e0db39322d55a33b6080ca5f292b776bb
Yvonne Ng <yvonne@tradegecko.com>
2018-07-21 20:54:12 +0800

    Stub profiles in json and add `profile` model

---
+++ app/models/profile.js
@@ -0,0 +1,12 @@
+import Model from 'ember-data/model';
+import attr from 'ember-data/attr';
+
+export default Model.extend({
+  login: attr('string'),
+  name: attr('string'),
+  email: attr('string'),
+  company: attr('string'),
+  isHireable: attr('boolean'),
+  bio: attr('string'),
+  avatarUrl: attr('string'),
+});
