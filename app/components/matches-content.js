a42dd85a4b46283910941ab312f80ea3dd14fb93
Yvonne Ng <yvonne@tradegecko.com>
2018-07-22 11:06:56 +0800

    [REFACTOR] Move keyboardShortcuts

--- app/components/matches-content.js
+++ app/components/matches-content.js
@@ -2,14 +2,16 @@ import Component from '@ember/component';
 import { inject as injectService } from '@ember/service';
 import KeyboardShortcuts from 'ember-keyboard-shortcuts/mixins/component';
 
+const KEYBOARD_SHORTCUTS = Object.freeze({
+  up: 'toggleInfo',
+  down: 'toggleInfo',
+  left: 'pass',
+  right: 'like',
+});
+
 export default Component.extend(KeyboardShortcuts, {
   classNames: ['tender-matches', 'col', 'p-3', 'd-flex', 'flex-column'],
-  keyboardShortcuts: {
-    up: 'toggleInfo',
-    down: 'toggleInfo',
-    left: 'pass',
-    right: 'like',
-  },
+  keyboardShortcuts: KEYBOARD_SHORTCUTS,
   profileService: injectService('profile'),
 
   nextProfile() {
