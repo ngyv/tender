8ac085e35d6b2afaf47267122c01debf0ea0fa9f
Yvonne Ng <yvonne@tradegecko.com>
2018-07-25 21:23:20 +0800

    [ENHANCEMENT] Expose alignment and size in `svg-icon`

--- app/components/svg-icon.js
+++ app/components/svg-icon.js
@@ -1,4 +1,5 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 
 /**
  * @type {String} octicon type defined in https://octicons.github.com/
@@ -11,9 +12,19 @@ import Component from '@ember/component';
  * {{svg-icon type="triangle-left" label="Nah"}}
  */
 export default Component.extend({
-  classNames: ['tender-icon', 'd-flex', 'align-items-center', 'py-2', 'justify-content-center'],
+  classNames: ['tender-icon', 'd-flex', 'py-2'],
+  classNameBindings: ['xFlexClass', 'yFlexClass'],
   type: 'circle-slash',
   size: 24,
   sizeUnit: 'px',
   label: null,
+  xAlign: 'center',
+  yAlign: 'center',
+
+  xFlexClass: computed('xAlign', function() {
+    return `align-items-${this.get('xAlign')}`;
+  }),
+  yFlexClass: computed('yAlign', function() {
+    return `justify-content-${this.get('yAlign')}`;
+  }),
 });
