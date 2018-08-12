02e0078cc6ff01d27b3147cd2ab41f7e7f8cda60
Yvonne Ng <yvonne@tradegecko.com>
2018-08-12 09:57:37 +0800

    [LINT]

--- app/adapters/user.js
+++ app/adapters/user.js
@@ -2,7 +2,7 @@ import ApplicationAdapter from './application';
 
 export default ApplicationAdapter.extend({
   namespace: 'search',
-  buildURL(_modelName, id, snapshot, requestType, query){
+  buildURL(_modelName, id, snapshot, requestType, query) {
     return this._super('user', id, snapshot, requestType, query);
   },
   handleResponse (_status, _headers, payload) {
