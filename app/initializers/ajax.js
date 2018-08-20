584eeeff0eaf4950e5abe87e902434a9f575cc11
Yvonne Ng <yvonne@tradegecko.com>
2018-08-20 19:57:50 +0800

    Tweak ajax init

--- app/initializers/ajax.js
+++ app/initializers/ajax.js
@@ -1,12 +1,17 @@
 import $ from 'jquery';
 import config from '../config/environment';
 
+const GITHUB_URL = 'https://api.github.com';
+
 export function initialize(/* application */) {
   $.ajaxPrefilter(function(options, originalOptions, /* _jqXHR */) {
-    const githubApi = new RegExp('https://api.github.com');
+    const githubSearchApi = new RegExp(`${GITHUB_URL}/search`);
+    const githubApi = new RegExp(GITHUB_URL);
+
     if (githubApi.test(originalOptions.url)) {
+      const searchQuery = githubSearchApi.test(originalOptions.url) ? { q: 'type:user' } : {};
       const data = Object.assign( // order matters in this
-        { q: 'repos:>1' },    // default `q` that can be overriden
+        searchQuery,    // default `q` that can be overriden
         originalOptions.data,
         { client_id: config.GIT_CLIENT, client_secret: config.GIT_SECRET }, // last so it's not accidentally overriden
       );
